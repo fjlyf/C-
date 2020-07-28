cca567e8745a32df825a036599002afd5e6d8c13
fj <1744875537@qq.com>
2020-07-28 22:17:34 +0800

    树

--- 数据结构（5）+队列/tree.h
+++ 数据结构（5）+队列/tree.h
@@ -22,6 +22,8 @@ void BinTreeInit(BinTree *pbt);  //初始化
 void BinTreeCreate(BinTree *pbt);  
 void BinTreeCreate_1(BinTreeNode **pbt); 
 BinTreeNode* BinTreeCreate_2();
+void BinTreeCreateByStr(BinTree *pbt,const char *str);//通过字符串创建
+BinTreeNode* BinTreeCreateByStr_1(const char *str,int *pindex);
 //遍历
 void PreOrder(BinTree *bt);     //先序遍历
 void PreOrder_1(BinTreeNode *t);
@@ -38,12 +40,18 @@ int BinTreeHeight(BinTree *bt);
 int BinTreeHeight_1(BinTreeNode *t);
 //查询
 BinTreeNode* BinTreeFind(BinTree *bt, ElemType key);
+BinTreeNode* BinTreeFind_1(BinTreeNode *t, ElemType key);
 BinTreeNode* BinTreeParent(BinTree *bt, ElemType key);  //查询父结点
+BinTreeNode* BinTreeParent_1(BinTreeNode *t, ElemType key);
 //拷贝二叉树
-BinTreeNode* BinTreeCopy(BinTree *bt);
+void BinTreeCopy(BinTree *bt1, BinTree *bt2);   //bt2=bt1
+BinTreeNode* BinTreeCopy_1(BinTreeNode *t);
 //判断二叉树
 bool BinTreeEqual(BinTree *bt1, BinTree *bt2);
-
+bool BinTreeEqual_1(BinTreeNode *t1, BinTreeNode *t2);
+//摧毁二叉树
+void BinTreeDestory(BinTree *bt);
+void BinTreeDestory_1(BinTreeNode *t);
 ///////////////////////////////////////////////////////////////////
 //函数实现
 void BinTreeInit(BinTree *pbt)
@@ -87,6 +95,28 @@ BinTreeNode* BinTreeCreate_2()
 		bt->leftChild = BinTreeCreate_2();
 		bt->rightChild = BinTreeCreate_2();
 		return bt;
+	}
+}
+
+void BinTreeCreateByStr(BinTree *bt, const char *str)
+{
+	int index = 0;
+	bt->root = BinTreeCreateByStr_1(str,&index);
+}
+BinTreeNode* BinTreeCreateByStr_1(const char *str,int *pindex)
+{
+	if (str[*pindex] == '#' || str[*pindex] == '\0')
+		return NULL;
+	else
+	{
+		BinTreeNode *t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		assert(t != NULL);
+		t->data = str[*pindex];
+		(*pindex)++;
+		t->leftChild = BinTreeCreateByStr_1(str, pindex);
+		(*pindex)++;
+		t->rightChild = BinTreeCreateByStr_1(str, pindex);
+		return t;
 	}
 }
 
@@ -135,8 +165,39 @@ void PostOrder_1(BinTreeNode *t)
 //查找
 BinTreeNode* BinTreeFind(BinTree *bt, ElemType key)
 {
-
+	return BinTreeFind_1(bt->root, key);
 }
+
+BinTreeNode* BinTreeFind_1(BinTreeNode *t, ElemType key)
+{
+	BinTreeNode *p;
+	if (t == NULL || t->data == key)
+		return t;
+	p = BinTreeFind_1(t->leftChild, key);
+	if (p != NULL)
+		return p;
+	return BinTreeFind_1(t->rightChild, key);
+}
+
+BinTreeNode* BinTreeParent(BinTree *bt, ElemType key)
+{
+	return BinTreeParent_1(bt->root, key);
+}
+
+BinTreeNode* BinTreeParent_1(BinTreeNode *t, ElemType key)
+{
+	BinTreeNode *p;
+	if (t == NULL||t->data==key)
+		return NULL;
+	if ((t->leftChild!=NULL&&t->leftChild->data == key) 
+		|| (t->rightChild!=NULL&&t->rightChild->data == key))
+		return t;
+	p = BinTreeParent_1(t->leftChild, key);
+	if (p != NULL)
+		return p;
+	return BinTreeParent_1(t->rightChild, key);
+}
+
 //求二叉树的节点数
 int BinTreeCount(BinTree *bt)
 {
@@ -165,6 +226,60 @@ int BinTreeHeight_1(BinTreeNode *t)
 		int right_h = BinTreeHeight_1(t->rightChild);
 		return(left_h > right_h ? left_h : right_h);
 	}
+}
+
+//拷贝二叉树
+void BinTreeCopy(BinTree *bt1, BinTree *bt2)
+{
+	bt2->root = BinTreeCopy_1(bt1->root);
+}
+
+BinTreeNode* BinTreeCopy_1(BinTreeNode *t)
+{
+	if (t == NULL)
+		return NULL;
+	else
+	{
+		BinTreeNode *tmp = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+		assert(tmp != NULL);
+		tmp->data = t->data;
+
+		tmp->leftChild = BinTreeCopy_1(t->leftChild);
+		tmp->rightChild = BinTreeCopy_1(t->rightChild);
+		return tmp;
+	}
+}
+
+//判断二叉树
+bool BinTreeEqual(BinTree *bt1, BinTree *bt2)
+{
+	return BinTreeEqual_1(bt1->root, bt2->root);
+}
+bool BinTreeEqual_1(BinTreeNode *t1, BinTreeNode *t2)
+{
+	if (t1 == NULL&&t2 == NULL)
+		return true;
+	if (t1 == NULL || t2 == NULL)
+		return false;
+	if (t1->data == t2->data
+		&&BinTreeEqual_1(t1->leftChild, t2->rightChild)
+		&& BinTreeEqual_1(t1->rightChild, t2->rightChild))
+		return true;
+	return false;
+}
+
+//摧毁二叉树
+void BinTreeDestory(BinTree *bt)
+{
+	BinTreeDestory_1(bt->root);
+}
+void BinTreeDestory_1(BinTreeNode *t)
+{
+	if (t == NULL)
+		return;
+	BinTreeDestory_1(t->leftChild);
+	BinTreeDestory_1(t->rightChild);
+	free(t);
 }
 
 #endif /*_TREE_H_*/
