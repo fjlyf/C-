7ef3b3f82313c73f38e8d296dc431d76e2744538
fj <1744875537@qq.com>
2020-08-17 22:02:26 +0800

    习题

--- 青蛙跳台问题.c
+++ 青蛙跳台问题.c
@@ -5,7 +5,6 @@
 //题目：一只青蛙一次只能跳一级台阶，也可以跳两级。求该青蛙上一个n级台阶总共有多少种跳法（先后次序不同算不同的结果）
 //对于n个台阶来说，只能从n-1或者n-2的台阶跳上来，所以F（n）=F(n-1)+F(n-2),采用递归
 
-
 int Jump_Floor(int n)
 {
 	if (n <= 2)
@@ -14,7 +13,6 @@ int Jump_Floor(int n)
 		return Jump_Floor(n - 2) + Jump_Floor(n - 1);
 		
 }
-
 int Jump_Floor1(int n)
 {
 	int a = 1;
@@ -29,7 +27,6 @@ int Jump_Floor1(int n)
 	}
 	return c;
 }
-
 int main()
 {
 	int n = 0;
